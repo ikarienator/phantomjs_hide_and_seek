e7bc75aa0c04a65a1f4d1895a00dbbfee33b2c80
Bei Zhang <u689@intrafile.com>
2014-09-16 18:50:44 -0700

    Function.prototype.bind spoofing

--- 5.simpleFunctionBind.js
+++ 5.simpleFunctionBind.js
@@ -1,9 +1,14 @@
-Function.prototype.bind = function () {
-    var func = this;
-    var self = arguments[0];
-    var rest = [].slice.call(arguments, 1);
-    return function () {
-        var args = [].slice.call(arguments, 0);
-        return func.apply(self, rest.concat(args));
-    };
+page.onInitialized = function () {
+    page.evaluate(function () {
+        Function.prototype.bind = function () {
+            var func = this;
+            var self = arguments[0];
+            var rest = [].slice.call(arguments, 1);
+            return function () {
+                var args = [].slice.call(arguments, 0);
+                return func.apply(self, rest.concat(args));
+            };
+        };
+
+    });
 };
